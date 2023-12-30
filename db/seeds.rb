98029b64a81d45a75479fbea913ef667de4ca165
miura <urainyan0305@gmail.com>
2023-12-30 21:11:57 +0900

    [Fix]本番環境のDBにデータ追加するためにseedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 require "csv"
 
-# 都道府県のテストデータ作成
-CSV.foreach('db/csv/prefecture.csv', headers: true) do |row|
-  Prefecture.create(
-    name: row['name']
-  )
-end
-
-# # 水族館のテストデータ作成
-# CSV.foreach('db/csv/aquarium.csv', headers: true) do |row|
-#   Aquarium.create(
-#     name: row['name'],
-#     prefecture_id: row['prefecture_id'],
-#     address: row['address'],
-#     website: row['website']
+# # 都道府県のテストデータ作成
+# CSV.foreach('db/csv/prefecture.csv', headers: true) do |row|
+#   Prefecture.create(
+#     name: row['name']
 #   )
 # end
+
+# 水族館のテストデータ作成
+CSV.foreach('db/csv/aquarium.csv', headers: true) do |row|
+  Aquarium.create(
+    name: row['name'],
+    prefecture_id: row['prefecture_id'],
+    address: row['address'],
+    website: row['website']
+  )
+end
