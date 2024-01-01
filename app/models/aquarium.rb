b7ae78b84a024a12fa85d8f5361ed53499057d1a
miura <urainyan0305@gmail.com>
2024-01-02 02:46:57 +0900

    [Add]postモデルと各モデルの関連付け、投稿画面作成、投稿機能実装

--- app/models/aquarium.rb
+++ app/models/aquarium.rb
@@ -1,3 +1,4 @@
 class Aquarium < ApplicationRecord
   belongs_to :prefecture
+  has_many :posts
 end
--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,4 +1,5 @@
 class Fish < ApplicationRecord
   belongs_to :location
   has_many :north_appearances, dependent: :destroy
+  has_many :posts
 end
