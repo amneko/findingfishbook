4c327c0208cb4a050ced037d2a23e001b52e645b
miura <urainyan0305@gmail.com>
2024-01-03 22:30:27 +0900

    [Fix]importmapにhotwireの読み込み設定追記

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,7 @@
 pin "application", preload: true
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.1/js/all.js"
 pin "preview", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
