10c2c3da6ec6f15dee30007ff3c3a2dce9191e3e
miura <urainyan0305@gmail.com>
2024-01-04 03:03:37 +0900

    [Fix]本番環境に出現情報のデータを追加するためのseed設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,30 +25,30 @@ require "csv"
 # end
 
 # # 魚のテストデータ作成
-CSV.foreach('db/csv/fish.csv', headers: true) do |row|
-  Fish.create(
-    name: row['name'],
-    location_id: row['location_id'],
-    selling_price_tanuki: row['selling_price_tanuki'],
-    selling_price_justin: row['selling_price_justin'],
-    image: row['image']
-  )
-end
-
-# # 出現情報のテストデータ作成
-# CSV.foreach('db/csv/north_appearance.csv', headers: true) do |row|
-#   NorthAppearance.create(
-#     fish_id: row['fish_id'],
-#     north_month: row['north_month'],
-#     latenight_starttime: row['latenight_starttime'],
-#     latenight_endtime: row['latenight_endtime'],
-#     morning_starttime: row['morning_starttime'],
-#     morning_endtime: row['morning_endtime'],
-#     noon_starttime: row['noon_starttime'],
-#     noon_endtime: row['noon_endtime'],
-#     evening_starttime: row['evening_starttime'],
-#     evening_endtime: row['evening_endtime'],
-#     night_starttime: row['night_starttime'],
-#     night_endtime: row['night_endtime']
+# CSV.foreach('db/csv/fish.csv', headers: true) do |row|
+#   Fish.create(
+#     name: row['name'],
+#     location_id: row['location_id'],
+#     selling_price_tanuki: row['selling_price_tanuki'],
+#     selling_price_justin: row['selling_price_justin'],
+#     image: row['image']
 #   )
 # end
+
+# 出現情報のテストデータ作成
+CSV.foreach('db/csv/north_appearance.csv', headers: true) do |row|
+  NorthAppearance.create(
+    fish_id: row['fish_id'],
+    north_month: row['north_month'],
+    latenight_starttime: row['latenight_starttime'],
+    latenight_endtime: row['latenight_endtime'],
+    morning_starttime: row['morning_starttime'],
+    morning_endtime: row['morning_endtime'],
+    noon_starttime: row['noon_starttime'],
+    noon_endtime: row['noon_endtime'],
+    evening_starttime: row['evening_starttime'],
+    evening_endtime: row['evening_endtime'],
+    night_starttime: row['night_starttime'],
+    night_endtime: row['night_endtime']
+  )
+end
