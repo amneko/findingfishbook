dcea11d9116a4b77a4a162841fce97a1305f0319
miura <urainyan0305@gmail.com>
2024-01-05 02:27:34 +0900

    [Fix]Vision APIのサービスアカウントキー読み込み設定の微修正

--- lib/vision.rb
+++ lib/vision.rb
@@ -3,34 +3,35 @@ require 'json'
 require 'net/https'
 require "google/cloud/vision/v1"
 
-keyfile_json_content = ENV['GOOGLE_APPLICATION_CREDENTIALS']
-
-require 'tempfile'
-keyfile = Tempfile.new('google_credentials')
-keyfile.write(keyfile_json_content)
-keyfile.rewind
-
-ENV['GOOGLE_APPLICATION_CREDENTIALS'] = keyfile.path
-
 module Vision
   class << self
     def image_analysis(post_image)
-      image_path = post_image
-      image_annotator = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
-      response = image_annotator.label_detection(
-        image:       image_path,
-        max_results: 10
-      )
+      keyfile_json_content = ENV['GOOGLE_APPLICATION_CREDENTIALS']
 
-      response.responses.each do |res|
-        res.label_annotations.each do |label|
-          return true if label.description.include?('Fish')
+      require 'tempfile'
+      keyfile = Tempfile.new('google_credentials')
+      begin
+        keyfile.write(keyfile_json_content)
+        keyfile.rewind
+        ENV['GOOGLE_APPLICATION_CREDENTIALS'] = keyfile.path
+
+        image_path = post_image
+        image_annotator = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
+        response = image_annotator.label_detection(
+          image:       image_path,
+          max_results: 10
+        )
+
+        response.responses.each do |res|
+          res.label_annotations.each do |label|
+            return true if label.description.include?('Fish')
+          end
         end
+        false
+      ensure
+        keyfile.close
+        keyfile.unlink
       end
-      false
     end
   end
 end
-
-keyfile.close
-keyfile.unlink
