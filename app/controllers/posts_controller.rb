c72a2e737c42edd3de97a15cea3d99ed14b551e6
miura <urainyan0305@gmail.com>
2024-01-05 23:55:35 +0900

    [Add]投稿編集画面・機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.build(post_params)
 
@@ -32,6 +36,34 @@ class PostsController < ApplicationController
       flash.now[:danger] = t('.fish_fail')
       respond_to do |format|
         format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('flash', partial: 'shared/flash_message', locals: { flash: { danger: flash.now[:danger] } })
+        end
+      end
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if params[:post][:post_image].present?
+      post_image = File.open(params[:post][:post_image].tempfile)
+      result = Vision.image_analysis(post_image)
+    else
+      result = true
+    end
+
+    if result == true
+      if @post.update(post_params)
+        redirect_to post_path(@post), success: t('.success')
+      else
+        flash.now[:danger] = t('.fail')
+        render :edit, status: :unprocessable_entity
+      end
+    elsif result == false
+      flash.now[:danger] = t('.fish_fail')
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
         format.turbo_stream do
           render turbo_stream: turbo_stream.update('flash', partial: 'shared/flash_message', locals: { flash: { danger: flash.now[:danger] } })
         end
