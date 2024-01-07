4449583770fcea0f1d13300068eecf1ecb91d438
miura <urainyan0305@gmail.com>
2024-01-07 10:17:38 +0900

    [Add]今釣れる魚表示機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def fish_to_catch_now
+    current_month = Time.now.strftime("%m")
+    current_time = Time.now.strftime("%H:%M")
+  
+    matching_data = NorthAppearance.where(north_month: current_month)
+                                   .where(
+                                     "(? BETWEEN latenight_starttime AND latenight_endtime) OR
+                                     (? BETWEEN morning_starttime AND morning_endtime) OR
+                                     (? BETWEEN noon_starttime AND noon_endtime) OR
+                                     (? BETWEEN evening_starttime AND evening_endtime) OR
+                                     (? BETWEEN night_starttime AND night_endtime)",
+                                     current_time, current_time, current_time, current_time, current_time
+                                   )
+                                   .pluck(:fish_id)
+  
+    fishes = Fish.where(id: matching_data).includes(:location)
+    fishes.group_by { |fish| fish.location }
+  end
 end
