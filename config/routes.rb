726c578be2305f16ed900c1ca002c72eea522959
miura <urainyan0305@gmail.com>
2024-01-07 23:33:53 +0900

    [Add]ユーザー削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: %i[new create destroy]
   resources :aquariums
   resources :fishes
   resources :posts
