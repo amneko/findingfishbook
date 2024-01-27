2aa816a2df48eed51d953fe046656ce86db1a3e2
miura <urainyan0305@gmail.com>
2024-01-27 19:48:26 +0900

    [Fix]rubocopによるヘルパーのリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,31 @@
+# frozen_string_literal: true
+
+# ApplicationHelper
 module ApplicationHelper
   def fish_to_catch_now
-    current_month = Time.zone.now.strftime("%m")
-    current_time = Time.zone.now.strftime("%H:%M")
-  
-    matching_data = NorthAppearance.where(north_month: current_month)
-                                   .where(
-                                     "(? BETWEEN latenight_starttime AND latenight_endtime) OR
-                                     (? BETWEEN morning_starttime AND morning_endtime) OR
-                                     (? BETWEEN noon_starttime AND noon_endtime) OR
-                                     (? BETWEEN evening_starttime AND evening_endtime) OR
-                                     (? BETWEEN night_starttime AND night_endtime)",
-                                     current_time, current_time, current_time, current_time, current_time
-                                   )
-                                   .pluck(:fish_id)
-  
+    current_month = Time.zone.now.strftime('%m')
+    current_time = Time.zone.now.strftime('%H:%M')
+    matching_data = matching_data(current_month, current_time)
+    group_fishes_by_location(matching_data)
+  end
+
+  private
+
+  def matching_data(month, time)
+    NorthAppearance.where(north_month: month)
+                   .where(
+                     "(? BETWEEN latenight_starttime AND latenight_endtime) OR
+                     (? BETWEEN morning_starttime AND morning_endtime) OR
+                     (? BETWEEN noon_starttime AND noon_endtime) OR
+                     (? BETWEEN evening_starttime AND evening_endtime) OR
+                     (? BETWEEN night_starttime AND night_endtime)",
+                     time, time, time, time, time
+                   )
+                   .pluck(:fish_id)
+  end
+
+  def group_fishes_by_location(matching_data)
     fishes = Fish.where(id: matching_data).includes(:location)
-    fishes.group_by { |fish| fish.location }
+    fishes.group_by(&:location)
   end
 end
--- app/helpers/aquarium_maps_helper.rb
+++ app/helpers/aquarium_maps_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# AquariumMapsHelper
 module AquariumMapsHelper
 end
