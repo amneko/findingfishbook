ebb9bf52bff627b76e695af2315be42ad02fa3f9
miura <urainyan0305@gmail.com>
2024-01-28 20:14:15 +0900

    [Fix]本番環境でのメール送信設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: ENV['RAILS_HOST'] }
+  config.action_mailer.default_url_options = { protocol: 'https', host: ENV['RAILS_HOST'] }
   config.action_mailer.perform_deliveries = true
 
   # メール送信方法をSMTPに設定
