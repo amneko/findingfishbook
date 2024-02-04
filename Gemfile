8a8326ec8937ed78e47e24968d7d050dcf3f6881
miura <urainyan0305@gmail.com>
2024-02-04 21:30:52 +0900

    [Add]OGP追加

--- Gemfile
+++ Gemfile
@@ -83,6 +83,8 @@ gem 'config'
 
 gem 'enum_help'
 
+gem "meta-tags"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,33 @@
 
 # ApplicationHelper
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Finding Fish Book',
+      title: 'ゲームに出現する魚と実在の水族館をリンクさせるサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: '『あつまれどうぶつの森』に出現する魚の写真と実際に見ることができる水族館を投稿できます。',
+      keywords: '魚,水族館',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ffb_ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@findingfishbook',
+        image: image_url('ffb_ogp.png')
+      }
+    }
+  end
+
   def fish_to_catch_now
     current_month = Time.zone.now.strftime('%m')
     current_time = Time.zone.now.strftime('%H:%M')
